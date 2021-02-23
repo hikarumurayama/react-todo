06c3356d94edd9f2b7881afb719d602c2ed18c2b
村山光 <73589927+hikarumurayama@users.noreply.github.com>
2021-02-23 13:26:06 +0900

    component-uncompleteTodos 未完了のTODOをコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { InputTodo } from "./components/InputTodo";
+import { UncompleteTodo } from "./components/UncompleteTodo";
 
 export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
@@ -46,30 +47,11 @@ export const App = () => {
         onChange={onChangeText}
         onClick={onClickAdd}
       />
-      {/* <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div> */}
-
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-                {/* 関数が即時実行されないようにアロー関数にする */}
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <UncompleteTodo
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
